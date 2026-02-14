417148329e28445a1c2ec605f3f2f4762eb1f51d
shubhammankaresknit-creator <shubhammankare.skn.it@gmail.com>
2026-02-14 11:32:08 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿//pattern *
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace parctical
+//{
+//    internal class Class1
+//    {
+//        public static void Main(string[] args)
+//        {
+
+//            int n = 3;
+
+
+            
+//            for (int c = 1; c <= n; c++)
+//            {
+//                for (int m = 1; m <= c; m++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+
+           
+//            for (int c = n - 1; c >= 1; c--)
+//            {
+//                for (int m = 1; m <= c; m++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
